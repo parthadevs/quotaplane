b1eb38b178de02c1e71f23c6a65ca170a447ee5b
Partha Sarker <parthasarker442@gmail.com>
2026-09-10 00:49:28 +0600

    (api) auth module login use case implementation complate with refresh token

---
+++ apps/api/src/modules/auth/application/use-cases/logout.use-case.ts
@@ -0,0 +1,23 @@
+import { Inject, Injectable } from "@nestjs/common";
+import type { ITokenService } from '../ports/token.service.interface';
+import type { IRefreshTokenRepository } from '../../domain/repositories/refresh-token.repository.interface';
+
+@Injectable()
+export class LogoutUseCase {
+    constructor(
+        @Inject('ITokenService') private readonly tokenService: ITokenService,
+        @Inject('IRefreshTokenRepository') private readonly refreshTokenRepository: IRefreshTokenRepository,
+    ) {}
+
+    async execute(token: string) {
+        const storedToken = await this.refreshTokenRepository.findByTokenHash(
+            this.tokenService.hashRefreshToken(token),
+        );
+        if (storedToken && !storedToken.isRevoked()) {
+            await this.refreshTokenRepository.revoke(storedToken.id);
+        }
+
+        return { message: 'Logged out successfully' };
+    }
+
+}
